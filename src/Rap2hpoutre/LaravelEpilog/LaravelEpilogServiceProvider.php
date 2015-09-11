838874fc617d10e66c30adea530da7a4b51e2730
raph <rap2hpoutre@users.noreply.github.com>
2015-09-11 16:58:01 +0200

    Update LaravelEpilogServiceProvider.php

--- src/Rap2hpoutre/LaravelEpilog/LaravelEpilogServiceProvider.php
+++ src/Rap2hpoutre/LaravelEpilog/LaravelEpilogServiceProvider.php
@@ -15,35 +15,37 @@ class LaravelEpilogServiceProvider extends ServiceProvider
     {
         $logger = \Log::getMonolog();
 
-		$logger->pushProcessor(function ($record) {
-			if(\Auth::check()) {
-				$user_info = "\n---";
-				$user_info .= "\n" . 'User #' . Auth::user()->id . ' (' . Auth::user()->email . ')';
-				$user_info .= ' - IP: ' . $_SERVER['REMOTE_ADDR'];
-				$user_info .= "\n" . $_SERVER['REQUEST_METHOD'] . " " .url($_SERVER['REQUEST_URI']);
-				if (isset($_SERVER['HTTP_REFERER'])) $user_info .= "\nReferer: " . $_SERVER['HTTP_REFERER'];
-				$user_info .= "\n---";
-				if (strpos($record['message'], "\n")) {
-					$record['message'] = preg_replace("/\n/", $user_info . "\n", $record['message'], 1);
-				} else {
-					$record['message'] .= $user_info . "\n";
-				}
-			}
-			return $record;
-		});
+        $logger->pushProcessor(function ($record) {
+            if (\Auth::check()) {
+                $user_info = "\n---";
+                $user_info .= "\n" . 'User #' . Auth::user()->id . ' (' . Auth::user()->email . ')';
+                $user_info .= ' - IP: ' . $_SERVER['REMOTE_ADDR'];
+                $user_info .= "\n" . $_SERVER['REQUEST_METHOD'] . " " . url($_SERVER['REQUEST_URI']);
+                if (isset($_SERVER['HTTP_REFERER'])) {
+                    $user_info .= "\nReferer: " . $_SERVER['HTTP_REFERER'];
+                }
+                $user_info .= "\n---";
+                if (strpos($record['message'], "\n")) {
+                    $record['message'] = preg_replace("/\n/", $user_info . "\n", $record['message'], 1);
+                } else {
+                    $record['message'] .= $user_info . "\n";
+                }
+            }
+            return $record;
+        });
 
-		if (app()->environment('TODO')) {
-			$slackHandler = new \Monolog\Handler\SlackHandler(
-				'TODO',
-				'TODO',
-				'TODO',
-				true,
-				':skull:',
-				\Monolog\Logger::ERROR
-			);
-			$logger->pushHandler($slackHandler);
-		}
-		
+        if (app()->environment('TODO')) {
+            $slackHandler = new \Monolog\Handler\SlackHandler(
+                'TODO',
+                'TODO',
+                'TODO',
+                true,
+                ':skull:',
+                \Monolog\Logger::ERROR
+            );
+            $logger->pushHandler($slackHandler);
+        }
+
     }
 
     /**
