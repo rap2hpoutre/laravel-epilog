ef85ec81ec82749804bc2fc2156cbacf17c1a7de
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-09-11 19:32:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Rap2hpoutre/LaravelEpilog/LaravelEpilogServiceProvider.php
+++ src/Rap2hpoutre/LaravelEpilog/LaravelEpilogServiceProvider.php
@@ -14,19 +14,25 @@ class LaravelEpilogServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes(array(
-            __DIR__.'/../../config/config.php' => config_path('epilog.php')
+            __DIR__ . '/../../config/config.php' => config_path('epilog.php')
         ));
 
         $logger = \Log::getMonolog();
 
-        $logger->pushProcessor(function ($record) {
+        $logger->pushProcessor(function($record) {
             $info = "\n---\n";
             if (\Auth::check()) {
                 $info .= 'User #' . \Auth::user()->id . ' (' . \Auth::user()->email . ') - ';
             }
-            if (isset($_SERVER['REMOTE_ADDR'])) $info .= 'IP: ' . $_SERVER['REMOTE_ADDR'];
-            if (isset($_SERVER['REQUEST_URI'])) $info .= "\n" . $_SERVER['REQUEST_METHOD'] . " " . url($_SERVER['REQUEST_URI']);
-            if (isset($_SERVER['HTTP_REFERER'])) $info .= "\nReferer: " . $_SERVER['HTTP_REFERER'];
+            if (isset($_SERVER['REMOTE_ADDR'])) {
+                $info .= 'IP: ' . $_SERVER['REMOTE_ADDR'];
+            }
+            if (isset($_SERVER['REQUEST_URI'])) {
+                $info .= "\n" . $_SERVER['REQUEST_METHOD'] . " " . url($_SERVER['REQUEST_URI']);
+            }
+            if (isset($_SERVER['HTTP_REFERER'])) {
+                $info .= "\nReferer: " . $_SERVER['HTTP_REFERER'];
+            }
             $info .= "\n---";
             if (strpos($record['message'], "\n")) {
                 $record['message'] = preg_replace("/\n/", $info . "\n", $record['message'], 1);
